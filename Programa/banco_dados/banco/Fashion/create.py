1e841703aaa81b5b73bb7b7dc6c84524c3c137d7
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-02 20:16:33 -0300

    atualização

--- Programa/banco_dados/banco/Fashion/create.py
+++ Programa/banco_dados/banco/Fashion/create.py
@@ -1,19 +1,20 @@
 import sqlite3
 
 
-def criacao_camisa():
+def criacao_produto():
     conexao = sqlite3.connect("fashion.db")
     
     cursor = conexao.cursor()
     
     cursor.execute('''
-                CREATE TABLE Camisas(
-                    id_camisa INTEGER PRIMARY KEY AUTOINCREMENT,
+                CREATE TABLE Produto(
+                    id_prod INTEGER PRIMARY KEY AUTOINCREMENT,
+                    tipo VARCHAR(20),
                     marca VARCHAR(50),
                     tamanho INTEGER,
                     cor VARCHAR(20),
                     genero VARCHAR(20),
-                    preco REAL,
+                    preco_unitario FLOAT,
                     quantidade INTEGER,
                     promocao BOOL,
                     descricao VARCHAR(100)
@@ -23,73 +24,47 @@ def criacao_camisa():
     conexao.commit()
     conexao.close()
 
-def criacao_calca():
+def criacao_informacoes_venda():
     conexao = sqlite3.connect("fashion.db")
     
     cursor = conexao.cursor()
     
     cursor.execute('''
-                CREATE TABLE Calcas(
-                    id_calcas INTEGER PRIMARY KEY AUTOINCREMENT,
-                    marca VARCHAR(50),
-                    tamanho INTEGER,
-                    cor VARCHAR(20),
-                    genero VARCHAR(20),
-                    preco REAL,
-                    quantidade INTEGER,
-                    promocao BOOL,
-                    descricao VARCHAR(100)
-                )
-    ''')
-    
-    conexao.commit()
-    conexao.close()
-
-
-def criacao_calca():
-    conexao = sqlite3.connect("fashion.db")
-    
-    cursor = conexao.cursor()
-    
-    cursor.execute('''
-                CREATE TABLE Calcas(
-                    id_calcas INTEGER PRIMARY KEY AUTOINCREMENT,
-                    marca VARCHAR(50),
-                    tamanho INTEGER,
-                    cor VARCHAR(20),
-                    genero VARCHAR(20),
-                    preco REAL,
-                    quantidade INTEGER,
-                    promocao BOOL,
-                    descricao VARCHAR(100)
-                )
-    ''')
-    
-    conexao.commit()
-    conexao.close()
-
-def criacao_informacoes_vendas():
-    conexao = sqlite3.connect("fashion.db")
-    
-    cursor = conexao.cursor()
-    
-    cursor.execute('''
-                CREATE TABLE Informacoes_vendas(
+                CREATE TABLE Venda(
                     id_venda INTEGER PRIMARY KEY AUTOINCREMENT,
-                    marca VARCHAR(50),
-                    tamanho INTEGER,
-                    cor VARCHAR(20),
-                    preco REAL,
-                    quantidade INTEGER,
-                    preco_total REAL,
-                    data_venda DATETIME
+                    data_venda DATETIME,
+                    preco_unitario FLOAT,
+                    quantidade_vendida INTEGER,
+                    preco_total FLOAT
                 )
     ''')
     
     conexao.commit()
     conexao.close()
+
+
+def criacao_pessoa_comprante():
+    conexao = sqlite3.connect("fashion.db")
+    
+    cursor = conexao.cursor()
+    
+    cursor.execute('''
+                CREATE TABLE Pessoa(
+                    id_pessoa INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome VARCHAR(100),
+                    CPF VARCHAR(20),
+                    id_prodfk INTEGER,
+                    id_vendafk INTEGER,
+                    FOREIGN KEY (id_prodfk) REFERENCES Produto(id_prod),
+                    FOREIGN KEY (id_vendafk) REFERENCES Venda(id_venda)
+                )
+    ''')
+    
+    conexao.commit()
+    conexao.close()
+
 
 if __name__ == "__main__":
-    criacao_camisa()
-    criacao_calca()
-    criacao_informacoes_vendas()
+    criacao_produto()
+    criacao_informacoes_venda()
+    criacao_pessoa_comprante()
