ac553d396569ca520a4b5954f010bf558482357d
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-11-09 13:03:38 -0300

    atualização

--- Programa/banco_dados/banco/Fashion/create.py
+++ Programa/banco_dados/banco/Fashion/create.py
@@ -23,47 +23,8 @@ def criacao_produto():
     
     conexao.commit()
     conexao.close()
-
-def criacao_informacoes_venda():
-    conexao = sqlite3.connect("fashion.db")
     
-    cursor = conexao.cursor()
-    
-    cursor.execute('''
-                CREATE TABLE Venda(
-                    id_venda INTEGER PRIMARY KEY AUTOINCREMENT,
-                    data_venda DATETIME,
-                    preco_unitario FLOAT,
-                    quantidade_vendida INTEGER,
-                    preco_total FLOAT
-                )
-    ''')
-    
-    conexao.commit()
-    conexao.close()
-
-
-def criacao_pessoa_comprante():
-    conexao = sqlite3.connect("fashion.db")
-    
-    cursor = conexao.cursor()
-    
-    cursor.execute('''
-                CREATE TABLE Pessoa(
-                    id_pessoa INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nome_pessoa VARCHAR(100),
-                    id_prodfk INTEGER,
-                    id_vendafk INTEGER,
-                    FOREIGN KEY (id_prodfk) REFERENCES Produto(id_prod),
-                    FOREIGN KEY (id_vendafk) REFERENCES Venda(id_venda)
-                )
-    ''')
-    
-    conexao.commit()
-    conexao.close()
 
 
 if __name__ == "__main__":
     criacao_produto()
-    criacao_informacoes_venda()
-    criacao_pessoa_comprante()
